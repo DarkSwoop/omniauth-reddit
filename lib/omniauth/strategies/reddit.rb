fc4312ac2fbcf9d6a5103fa372dc80624cccb1c7
Benjamin Behr <benny@digitalbehr.de>
2016-01-24 00:06:10 +0100

    don't use the query_string
    In certain situations the callback_url returns the full url including the query_string. Reddit doesn't strip the query_string when supplied as callback_url not finding the correct access_token in their database

--- lib/omniauth/strategies/reddit.rb
+++ lib/omniauth/strategies/reddit.rb
@@ -51,6 +51,9 @@ module OmniAuth
         ua.downcase =~ Regexp.new(MOBILE_USER_AGENTS)
       end
 
+      def callback_url
+        full_host + script_name + callback_path
+      end
 
     end
   end
